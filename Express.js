5cb84520df4605468fa086d0df1430bf127a8025
jagdish-18 <shiyaljagdish18@gmail.com>
2024-08-09 11:49:30 +0530

    09-08-24

--- Express.js
+++ Express.js
@@ -1,87 +1,69 @@
 const express = require('express');
 const server = express();
-
-//middleware
-
-// Built-in middleware
-//1-> express.json() ->raw / json formate
-//2-> express.urlencoded() -> form
-//3-> express.static()
-
-//1-> express.json() ->raw / json formate
-// server.use(express.json());
-// function middleware(req, res, next) {
-//     console.log(req.body);
-//     if (req.body.age >= 18) {
-//         console.log("success");
-//         next();
-//     } else {
-//         return res.json({ message: "Inccorect Way!!!!" });
-//     }
-// }
-
-// 2-> express.urlencoded() -> form
-// server.use(express.urlencoded({extended : true}))
-// function middleware(req, res, next) {
-//     console.log(req.body);
-//     if (req.body.age >= 18) {
-//         console.log("success");
-//         next();
-//     } else {
-//         return res.json({ message: "Inccorect Way!!!!" });
-//     }
-// }
-
-//3-> express.static()
-// server.use("/hey",express.static('public'));
-// function middleware(req, res, next) {
-//         console.log(req.body);
-//         if (req.body.age >= 18) {
-//             console.log("success");
-//             next();
-//         } else{
-//             return res.json({ message: "Inccorect Way!!!!" });
-//         }
-//     }
-
-// server.get('/', middleware ,(req,res)=>{
-//     res.write("<h1>Welcome to server</h1>")
-//     res.end()
-// })
-
-// server.get('/', (req , res) =>{
-//     res.json({message:'hello'});
-// })
-
-
- // third party middleware
- 
-//  let loggerfunction = (req , res , next) =>{
-//     console.log(req.url , "\t" , req.method , '\t');
-//     next();
-//  }
-
-//  server.use(loggerfunction)
-
+const product = require('./product.json');
 const morgan = require('morgan');
+const user = require('./user.json')
 
-// server.use(morgan('dev'))
-// server.use(morgan('tiny'))
-server.use(morgan('combined'))
-
+server.use(express.json());
+server.use(express.urlencoded({extended: "true"}));
+server.use(morgan('dev'));
 
 server.get('/', (req , res) =>{
-    res.json({message:'hello'});
+    res.json(product)
 })
 
-server.post('/', (req , res) =>{
-    res.json({message:'hello'});
+//CRUD
+
+//Add New Product -Create
+server.post("/product" , (req , res) =>{
+    // console.log(req.body);
+    product.push(req.body);
+    res.json({product : req.body , message :`Product Added Success`})
+    
 })
+
+// Get All Product  - Read
+server.get("/product" , (req ,res) =>{
+    res.json(product)
+})
+
+// Get Single Product -Read
+// server.get("/product/:id" , (req ,res) =>{
+//     let id = +req.params.id;
+//     let item = product.find((product) => product.id === id);
+//     res.json(item);
+// })
+
 
 server.listen(6000 , () =>{
      console.log(`server start at http://localhost:6000`);
         
     })
 
+
+ server.get("/userdata" , (req ,res) =>{
+    res.json(user);
+ })   
+
+
+ server.post('/user' , (req ,res) =>{
+    user.push(req.body);
+    res.json({user : req.body ,message: `user Added Succesfully`})
+ })
+
+ server.get('/user' , (req ,res) =>{
+    res.json(user)
+ })
+
+ server.get("/user/:id" , (req ,res) =>{
+    let id = +req.params.id;
+    let item = user.find((user) => user.id === id);
+    res.json(item)
+ })
+
+ server.listen(7000 , () =>{
+    console.log(`server started at http://localhost:7000`);
+    
+ })
 
 
