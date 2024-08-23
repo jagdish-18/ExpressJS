3ccde7c98892648270f4a5d4ed4e2d520e0bade5
jagdish-18 <shiyaljagdish18@gmail.com>
2024-08-23 10:48:43 +0530

    23-08-24

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -48,3 +48,49 @@ exports.userProfile = async (req ,res) =>{
     res.status(500).json({message : "Internal server Error"})
   }
 }
+
+// Update User
+exports.updateUser = async (req ,res) =>{
+  try {
+    let user = req.user;
+    user = await User.findByIdAndUpdate(user._id , {$set : req.body} , {new : true});
+    res.status(202).json({user , message : "User Update Success"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message : "Internal Server Error"})
+  }
+}
+
+// delete user
+exports.deleteUser = async (req , res) =>{
+  try {
+    let user = req.user;
+    user = await User.findByIdAndUpdate(user._id , {$set : {isDelete: true}} , {new : true});
+    res.status(202).json({user , message : "User Delete Success"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message : "Internal Server Error....."});
+  }
+}
+
+// Change Password 
+exports.changePassword = async (req, res) => {
+  try {
+      const { email, oldPassword, newPassword} = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+      if (!isMatch) {
+          return res.status(400).json({ message: 'Incorrect old password' });
+      }
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
+      await user.save();
+      res.json({ message: 'Password changed successfully' });
+  } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" })
+  }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {registerUser , loginUser , userProfile} = require('../controller/user.controller');
+const {registerUser , loginUser , userProfile, updateUser , deleteUser , changePassword} = require('../controller/user.controller');
 const { verifyToken } = require('../helpers/tokenVerify');
 
 const userRoutes = express.Router();
@@ -7,6 +7,10 @@ const userRoutes = express.Router();
 userRoutes.post("/register" , registerUser);
 userRoutes.post("/login" , loginUser);
 
-userRoutes.get("/me" , verifyToken ,  userProfile)
+userRoutes.get("/me" , verifyToken ,  userProfile);
+userRoutes.put("/update-profile" , verifyToken ,  updateUser);
+userRoutes.delete("/delete-profile" , verifyToken ,  deleteUser);
+userRoutes.post("/change-password" , changePassword);
+
 
 module.exports = userRoutes;
